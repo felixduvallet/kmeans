3760c2e695e4d457d96d114f9de3152c9dd3a4cf
Felix Duvallet <felix.duvallet@epfl.ch>
2016-06-20 19:58:10 +0200

    more lints.

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -1,24 +1,50 @@
-//
-// Created by felixd on 6/18/16.
-//
+// Author: Felix Duvallet
 
-#include <map>
-#include <iostream>
 #include "kmeans.h"
-
 
 using namespace std;
 
-KMeans::KMeans(int k)
-        : k_(k) {
+KMeans::KMeans(int k, int max_iterations)
+        : num_clusters_(k),
+          max_iterations_(max_iterations) {
 
+}
+
+bool KMeans::init(const std::vector<Point> &points) {
+    points_.clear();
+
+    for (const auto &p : points) {
+        points_.push_back(p);
+    }
+
+    for (int idx = 0; idx < num_clusters_; ++idx) {
+        Point mean(points[idx]);
+        means_.push_back(mean);
+    }
+
+    return true;
+}
+
+bool KMeans::run() {
+    for (int iteration = 1; iteration <= max_iterations_; ++iteration) {
+        cout << "== KMeans iteration " << iteration << " == " << endl;
+        bool changed = assign();
+        update_means();
+
+        if(!changed) {
+            cout << "KMeans has converted after " << iteration
+            << " iterations." << endl;
+            return true;
+        }
+    }
+    return false;
 }
 
 int KMeans::findNearestCluster(const Point &point) {
     double min_dist = 1e12;
     int min_cluster = -1;
 
-    for (int idx = 0; idx < k_; ++idx) {
+    for (int idx = 0; idx < num_clusters_; ++idx) {
         const double dist = Point::distance(point, means_[idx]);
         if (dist < min_dist) {
             min_dist = dist;
@@ -31,73 +57,73 @@ int KMeans::findNearestCluster(const Point &point) {
 
 bool KMeans::assign() {
     bool changed = false;
-    // Assign each point to the nearest cluster.
-    int new_cluster;
+    // Assign each point to the nearest cluster: iterate over all points, find
+    // the nearest cluster, and assign.
     for (auto &point : points_) {
-        new_cluster = findNearestCluster(point);
-        // set changed to true if the cluster was updated. Note that we cannot inline
-        // (changed = changed || update) since the compiler will 'optimize out' the update step.
+        const int new_cluster = findNearestCluster(point);
+
+        // set changed to true if the cluster was updated. Note that we cannot
+        // inline (changed = changed || update) since the compiler will
+        // 'optimize out' the update step.
         bool ret = point.update(new_cluster);
         changed = changed || ret;
 
-        cout << "Assigned point " << point << " to cluster: " << new_cluster << endl;
+        cout << "Assigned point " << point << " to cluster: "
+             << new_cluster << endl;
     }
     return changed;
 }
 
 bool KMeans::update_means() {
-    cout <<"---- update means ----" << endl;
     // Compute each mean as the mean of the points in that cluster.
-    std::multimap<int, const Point *> point_cluster_map;
 
+    // First, compute a map of the cluster assignments. This prevents us from
+    // iterating over the data k times.
+    std::multimap<int, const Point *> point_cluster_map;
     for (const auto &point : points_) {
-        cout << "map[" << point.cluster_ << "]: Point:  " << point << endl;
-        point_cluster_map.insert(std::pair<int, const Point *>(point.cluster_, &point));
+        // Map is cluster_index -> Point*
+        auto pair = std::pair<int, const Point *>(point.cluster_, &point);
+        point_cluster_map.insert(pair);
     }
 
     // Iterate over each cluster, computing the mean.
-    for (int cluster_idx = 0; cluster_idx < k_; ++cluster_idx) {
-        computeClusterMean(point_cluster_map, cluster_idx, &means_[cluster_idx]);
+    for (int cluster_idx = 0; cluster_idx < num_clusters_; ++cluster_idx) {
+        computeClusterMean(point_cluster_map, cluster_idx,
+                           &means_[cluster_idx]);
     }
     return true;
 }
 
-void KMeans::computeClusterMean(const std::multimap<int, const Point *> &multimap, int k, Point *mean) {
+void KMeans::computeClusterMean(
+        const std::multimap<int, const Point *> &multimap,
+        int cluster,
+        Point *mean) {
+
+    // Zero-out the mean.
     for (int dim = 0; dim < mean->dimensions_; ++dim)
         mean->data_[dim] = 0.0;
 
-    // Find all the points in the given cluster, this returns an iterator pair (begin and end).
-    auto points_in_cluster = multimap.equal_range(k);
+    // Find all the points in the given cluster, this returns an iterator pair
+    // (begin and end).
+    auto in_cluster = multimap.equal_range(cluster);
     int num_points = 0;
-    cout << "[compute] have range " << " for cluster " << k << endl;
 
-    for (auto itr = points_in_cluster.first; itr != points_in_cluster.second; ++itr) {
+    // Compute the mean: sum over all points, then divide by the number.
+    for (auto itr = in_cluster.first; itr != in_cluster.second; ++itr) {
         mean->add(*(itr->second));
 
         num_points += 1;
-        cout << "  -- adding point: " << *(itr->second) << " .. " << *mean << endl;
     }
-    cout << " number of points:  " << num_points << endl;
 
-    // Divide by number of points in cluster.
     for (unsigned int idx = 0; idx < mean->dimensions_; ++idx) {
         mean->data_[idx] /= float(num_points);
     }
-    cout << "Have new mean:  " << *mean << endl;
 }
 
-bool KMeans::init(const std::vector<Point> &points) {
-    points_.clear();
-
-    for (const auto &p : points) {
-        points_.push_back(p);
+void KMeans::printMeans() {
+    for (const auto &mean : means_) {
+        cout << "Mean: " << mean << endl;
     }
-
-    for (int idx = 0; idx < k_; ++idx) {
-        Point mean(points[idx]);
-        means_.push_back(mean);
-    }
-
-    return true;
 }
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,20 @@
 using namespace std;
 
 int main() {
-
-
     Point p1(std::vector<double>{-2.0, 1.0});
-    cout << p1.data_[0] << endl;
     Point p2(std::vector<double>{-1.0, 3.0});
-
     Point p3(std::vector<double>{5.0, 2.0});
     Point p4(std::vector<double>{6.0, 4.0});
     Point p5(std::vector<double>{7.0, 2.0});
-
 
     std::vector<Point> points{p1, p2, p3, p4, p5};
 
     KMeans kmeans(2);
     kmeans.init(points);
 
-    for (const auto &mean : kmeans.means_) {
-        cout << "Mean: " << mean << endl;
-    }
+    kmeans.run();
+    kmeans.printMeans();
 
-    bool changed = false;
-    for (int iteration = 1; iteration <= 10; ++iteration) {
-        cout << "== KMeans iteration " << iteration << " == " << endl;
-        changed = kmeans.assign();
-        kmeans.update_means();
-
-        if(!changed) {
-            cout << "KMneans has converted after " << iteration << " iterations." << endl;
-            break;
-        }
-    }
-
-    cout << "Hello, World!" << endl;
+    cout << "Finished." << endl;
     return 0;
 }
